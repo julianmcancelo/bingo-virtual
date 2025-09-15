b2492aeecb2a831c00653b82217536ffa7cab65d
Julian Cancelo <jcancelo@ibeltran.com.ar>
2025-09-15 18:17:33 -0300

    --

--- bingo-virtual/server/server.js
+++ bingo-virtual/server/server.js
@@ -19,7 +19,7 @@
 
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
+const { Server } = require('socket.io');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const chalk = require('chalk');
